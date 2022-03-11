9d5093112cb8ecdff6e4051a59da8f75238fbc7d
alamin5g <alaminrakib4864@gmail.com>
2022-03-11 15:44:03 +0600

    calculate the odd number sum from 1 to n number

---
+++ evennumber.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+    int even = 1, odd, sum=0,i;
+    printf("Enter odd number to calculate the sum of 1 to n numbers: ");
+    scanf("%d", &odd);
+    even = odd%2;
+    while(even == 0){
+        printf("Your %d is not odd number, enter odd number: ", odd);
+        scanf("%d", &odd);
+        even++;
+        even = odd%2;
+    }
+
+    for(i=1; i<=odd; i+=2){
+        sum = sum+i;
+    }
+
+    printf("1 to %d\'s odd total sum is %d ", odd, sum);
+}
