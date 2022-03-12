7c2db19bae43cb82f5f5e6eed55d12d2de4a0339
alamin5g <alaminrakib4864@gmail.com>
2022-03-12 15:49:57 +0600

    2. Write a program to compute the perimeter and area of a circle with a given radius.

--- areaOfCircle.c
+++ areaOfCircle.c
@@ -4,12 +4,13 @@
 #define PI 3.1416 //constant value
 
 int main(){
-    float radious, area;
+    float radious, area, perimeter;
     printf("Enter radious value: ");
     scanf("%f", &radious);
 
     area = PI * radious * radious;
-    printf("The area of a circle is : %f", area);
-    
+    perimeter = 2 * radious * PI;
+    printf("The area of a circle is : %f\n", area);
+    printf("The perimeter of circle is : %f", perimeter);
     return 0;
 }
