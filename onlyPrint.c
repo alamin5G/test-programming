ba164c626e72c2792c3e86583d97f2b2fd38b617
alamin5g <alaminrakib4864@gmail.com>
2022-03-12 15:36:46 +0600

    1. Write a program to print your name, date of birth. and mobile number

---
+++ onlyPrint.c
@@ -0,0 +1,8 @@
+/*1. Write a program to print your name, date of birth. and mobile number*/
+
+#include <stdio.h>
+void main(){
+    printf("Md. Alamin\n");
+    printf("2 July, 1997\n");
+    printf("+8801822679672"); ///+88 uses for international purpose
+}
