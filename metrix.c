da291c3f489fb5faf2f9d2bb8fde26095d047669
alamin5g <alaminrakib4864@gmail.com>
2022-03-15 16:52:06 +0600

    47. Write a program to print a matrix using array.

---
+++ metrix.c
@@ -0,0 +1,26 @@
+/**
+ * @file metrix.c
+ * 47. Write a program to print a matrix using array.
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-03-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+void main(){
+    int arr[0][0], a, b, row, col;
+
+    printf("Enter matrix row and colum size: \n");
+    scanf("%d %d", &row, &col);
+    arr[row][col];
+    printf("The matrix is: \n");
+    for(a=1; a<=row; a++){
+        for(b=1; b<=col; b++){
+            printf("%d  ", b+a);
+        }
+        printf("\n");
+    }
+}
